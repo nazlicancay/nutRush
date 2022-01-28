bc59305cdff8103247e289fefbab3e3c7bbc2a21
Nazlıcan Çay <nazlican1306@gmail.com>
2022-01-28 17:52:54 +0300

    commit

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public List<GameObject> levels = new List<GameObject>();
+    public Transform targets;
+    public GameObject Player;
+
+    private void Awake()
+    {
+    }
     void Start()
     {
         
@@ -14,5 +22,25 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateLevel()
+    {
+        int index = PlayerPrefs.GetInt("LevelNum");
+
+        for(int i =0; i < levels.Count; i++)
+        {
+            if (i != index)
+            {
+                levels[i].SetActive(false);
+            }
+            else
+            {
+                levels[index].SetActive(true);
+                
+                //Player.transform.DOMove()
+            }
+        }
+       
     }
 }
